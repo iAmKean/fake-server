9ed94f2314e6bf0ef49914e3edfec0550c6b1829
keno <keno@thinkbitsolutions.com>
2024-04-11 10:29:54 +0800

    feat: added browser options

--- src/main/index.js
+++ src/main/index.js
@@ -6,43 +6,68 @@ import '../renderer/store'
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+	global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+	? `http://localhost:9080`
+	: `file://${__dirname}/index.html`
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
-  })
+function createWindow() {
+	/**
+	 * Initial window options
+	 */
 
-  mainWindow.loadURL(winURL)
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+	const { screen } = require('electron')
+	const primaryDisplay = screen.getPrimaryDisplay()
+	const { width, height } = primaryDisplay.workAreaSize
+
+	const windowOptions = {
+		minHeight: 720,
+		height: height,
+		x: 0,
+		y: 0,
+		useContentSize: true,
+		process: process,
+		width: width,
+		minWidth: 1024,
+		webPreferences: {
+			webSecurity: false,
+			nodeIntegration: true,
+		},
+		useContentSize: true,
+		alwaysOnTop: false,
+		minimizable: true,
+		maximizable: true,
+		closable: true,
+		frame: true,
+		skipTaskbar: false,
+		focusable: true,
+	};
+
+	mainWindow = new BrowserWindow(windowOptions)
+
+	mainWindow.loadURL(winURL)
+
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
+	if (mainWindow === null) {
+		createWindow()
+	}
 })
 
 /**
